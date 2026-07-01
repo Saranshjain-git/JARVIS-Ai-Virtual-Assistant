a0203c735acbd8a9cfb2190159c226270ac3df4f
Saranshjain-git <jainsaranshh@gmail.com>
2026-07-01 11:17:08 +0530

    Update Jarvis project

--- web/app.py
+++ web/app.py
@@ -1,6 +1,37 @@
 from flask import Flask, render_template, request, jsonify
+import os
+import sys
+import traceback
 
 app = Flask(__name__)
+
+# =========================
+# CHECK IF RUNNING ON RENDER
+# =========================
+
+IS_RENDER = os.environ.get("RENDER") == "true"
+
+jarvis = None
+
+if not IS_RENDER:
+    try:
+        BASE_DIR = os.path.dirname(
+            os.path.dirname(
+                os.path.abspath(__file__)
+            )
+        )
+
+        sys.path.append(BASE_DIR)
+
+        import main as jarvis
+        print("AFTER IMPORT")
+        print(jarvis)
+
+        print("Local JARVIS Loaded Successfully")
+
+    except Exception as e:
+        print("Jarvis Import Error:")
+        traceback.print_exc()
 
 # =========================
 # HOME
@@ -11,30 +42,59 @@ def home():
     return render_template("index.html")
 
 # =========================
-# COMMAND ROUTE
+# COMMAND
 # =========================
 
 @app.route("/command", methods=["POST"])
 def command():
 
-    data = request.get_json()
+    try:
 
-    user_command = data.get("command", "")
+        data = request.get_json()
 
-    print("Command:", user_command)
+        user_command = data.get("command", "")
 
-    return jsonify({
-        "status": "success",
-        "message": f"Command received: {user_command}\n\n⚠ Desktop features work in the local Windows version of JARVIS."
-    })
+        print("Command:", user_command)
+
+        # LOCAL WINDOWS
+        if jarvis is not None:
+
+            result = jarvis.handle_command(
+                user_command.lower()
+            )
+
+            return jsonify({
+                "status": "success",
+                "message": result if result else "Command Executed"
+            })
+
+        # RENDER
+        else:
+
+            return jsonify({
+                "status": "success",
+                "message": f"Command received: {user_command}\n\nDesktop automation works only in the local Windows version."
+            })
+
+    except Exception as e:
+
+        traceback.print_exc()
+
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        })
 
 # =========================
-# RUN APP
+# RUN
 # =========================
 
 if __name__ == "__main__":
+    print("STARTING FLASK")
+
     app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=False
-    )
+    host="127.0.0.1",
+    port=5001,
+    debug=False,
+    use_reloader=False
+)
